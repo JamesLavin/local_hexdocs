0c7b3424e4f89622d4f3a79458bf3d4ba67b28ff
mateusz <james@jameslavin.com>
2025-03-15 01:11:11 -0400

    delete commented-out, unused code

--- save_hexdocs.exs
+++ save_hexdocs.exs
@@ -3,10 +3,3 @@ Code.ensure_loaded(LocalHexdocs)
 
 LocalHexdocs.fetch_all()
 
-
-# libraries
-# |> Task.async_stream(fn lib -> IO.puts(~c(mix hex.docs fetch #{lib})) end)
-
-# |> Task.async_stream(fn lib -> System.shell("mix hex.docs fetch #{lib}", into: IO.stream()) end)
-# |> Task.async_stream(fn lib -> System.cmd("mix", ["hex.docs", "fetch", lib]) end)
-

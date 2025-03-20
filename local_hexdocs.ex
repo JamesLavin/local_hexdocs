2d4f4dc94dfc80fd5078cc0c04078a4dc8944c9d
mateusz <james@jameslavin.com>
2025-03-19 22:57:33 -0400

    create initial tests

--- local_hexdocs.ex
+++ local_hexdocs.ex
@@ -247,7 +247,7 @@ defmodule LocalHexdocs do
   end
 
   defp packages_files do
-    case Path.expand(".") |> Path.join("/packages") |> File.ls() do
+    case Path.expand("./packages") |> File.ls() do
       {:ok, []} ->
         packages_file()
 
@@ -339,7 +339,23 @@ defmodule LocalHexdocs do
   end
 
   defp hexpm_dir do
-    "~/.hex/docs/hexpm/" |> Path.expand()
+    if running_tests?() do
+      "./test_hexpm" |> Path.expand()
+    else
+      # script is executing normally
+      "~/.hex/docs/hexpm/" |> Path.expand()
+    end
+  end
+
+  def running_tests? do
+    "local_hexdocs/tests" == File.cwd!() |> path_end()
+  end
+
+  defp path_end(string_path) when is_binary(string_path) do
+    list = string_path |> String.split("/")
+    {last, rest} = list |> List.pop_at(-1)
+    {prev, _rest2} = rest |> List.pop_at(-1)
+    "#{prev}/#{last}"
   end
 
   # IMPROVE

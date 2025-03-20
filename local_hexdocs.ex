2d0cbbf0c2e187d458a39e80fbf3654e41ed80e8
mateusz <james@jameslavin.com>
2025-03-19 21:27:33 -0400

    document two more public functions; move all public funs to top of module; mark all other funs private

--- local_hexdocs.ex
+++ local_hexdocs.ex
@@ -25,11 +25,11 @@ defmodule LocalHexdocs do
 
   @mix_path :os.cmd(~c(which mix)) |> Path.expand() |> String.trim()
 
-  defp hexpm_dir do
-    "~/.hex/docs/hexpm/" |> Path.expand()
-  end
+  @doc """
+  Generates a list of package names with downloaded Hexdocs doc files.
 
-  # Called by `local_docs.exs list`
+  Called by `local_docs.exs list`
+  """
   def downloaded_packages do
     hexpm_dir()
     |> File.ls!()
@@ -41,7 +41,42 @@ defmodule LocalHexdocs do
     )
   end
 
-  # Called by `local_docs.exs versions`
+  @doc """
+  Finds all LocalHexdocs configuration files, generates and de-duplicates a list of all packages in the config
+  files, and then fetches each -- one by one to avoid overloading the Hexdocs.pm server or triggering rate limiting.
+  Saves each package's Hexdocs files in a directory like `~/.hex/docs/hexpm/`.
+
+  Called by `local_docs.exs get`
+  """
+  def fetch_all do
+    stream =
+      desired_packages()
+      |> Task.async_stream(fn lib -> :os.cmd(~c(#{@mix_path} hex.docs fetch #{lib})) end,
+        timeout: @timeout_ms,
+        max_concurrency: @max_concurrency
+      )
+
+    # Expected responses:
+    # "Failed to retrieve package information\nAPI rate limit exceeded for IP [my IP address]\n** (MatchError) no match of right hand side value: nil\n    (hex 2.1.1) lib/mix/tasks/hex.docs.ex:135: Mix.Tasks.Hex.Docs.find_package_latest_version/2\n    (hex 2.1.1) lib/mix/tasks/hex.docs.ex:99: Mix.Tasks.Hex.Docs.fetch_docs/2\n    (mix 1.17.3) lib/mix/task.ex:495: anonymous fn/3 in Mix.Task.run_task/5\n    (mix 1.17.3) lib/mix/cli.ex:96: Mix.CLI.run_task/2\n    /home/mateusz/.asdf/installs/elixir/1.17.3-otp-27/bin/mix:2: (file)"
+    # {:ok, ~c"Couldn't find docs for package with name neotoma or version 1.7.3\n"}
+    # {:ok, ~c"** (Mix) No package with name made_up_library\n"}
+    # {:ok, ~c"Docs already fetched: /home/mateusz/.hex/docs/hexpm/mox/1.2.0\n"}
+    # {:ok, ~c"Docs fetched: /home/mateusz/.hex/docs/hexpm/paginator/1.2.0\n"}
+
+    stream
+    |> Stream.take_while(fn resp -> !rate_limited?(resp) end)
+    |> Stream.each(&display_response/1)
+    |> Enum.to_list()
+    |> process_list()
+    |> IO.inspect(limit: :infinity, printable_limit: :infinity)
+  end
+
+  @doc """
+  Generates a list of package names with downloaded Hexdocs doc files, each with a list of all
+  downloaded package versions.
+
+  Called by `local_docs.exs versions`
+  """
   def display_downloaded_packages_with_versions do
     downloaded_packages_with_versions()
     |> IO.inspect(
@@ -49,10 +84,6 @@ defmodule LocalHexdocs do
       limit: :infinity,
       printable_limit: :infinity
     )
-  end
-
-  def package_name_plus_versions(name) do
-    {name |> String.to_atom(), package_versions(name)}
   end
 
   @doc """
@@ -89,6 +120,24 @@ defmodule LocalHexdocs do
     |> Enum.each(&delete_older_versions/1)
   end
 
+  @doc """
+  Finds and displays all packages with multiple Hexdocs versions
+
+  Called by `local_docs.exs multiple_versions`
+  """
+  def display_downloaded_packages_with_multiple_versions do
+    downloaded_packages_with_multiple_versions()
+    |> IO.inspect(
+      label: "Packages downloaded in #{hexpm_dir()} with multiple versions",
+      limit: :infinity,
+      printable_limit: :infinity
+    )
+  end
+
+  defp package_name_plus_versions(name) do
+    {name |> String.to_atom(), package_versions(name)}
+  end
+
   defp delete_older_versions(%{
          package: _package_name,
          delete: _versions_to_delete,
@@ -109,7 +158,7 @@ defmodule LocalHexdocs do
     |> Path.join([package_name, "/", version_string])
   end
 
-  def package_versions_to_remove do
+  defp package_versions_to_remove do
     grab_package_name = fn {package_name, _versions} -> package_name end
 
     downloaded_packages_with_multiple_versions()
@@ -118,7 +167,7 @@ defmodule LocalHexdocs do
     |> Enum.map(&versions_to_keep_and_delete/1)
   end
 
-  def versions_to_keep_and_delete(package_name) do
+  defp versions_to_keep_and_delete(package_name) do
     versions =
       package_name
       |> package_versions()
@@ -132,20 +181,6 @@ defmodule LocalHexdocs do
     delete_dirs = delete_vers |> Enum.map(&path_to_version(package_name, &1))
 
     %{package: package_name, keep: latest, delete: delete_vers, delete_dirs: delete_dirs}
-  end
-
-  @doc """
-  Finds and displays all packages with multiple Hexdocs versions
-
-  Called by `local_docs.exs multiple_versions`
-  """
-  def display_downloaded_packages_with_multiple_versions do
-    downloaded_packages_with_multiple_versions()
-    |> IO.inspect(
-      label: "Packages downloaded in #{hexpm_dir()} with multiple versions",
-      limit: :infinity,
-      printable_limit: :infinity
-    )
   end
 
   defp downloaded_packages_with_versions do
@@ -191,7 +226,7 @@ defmodule LocalHexdocs do
     |> File.ls!()
   end
 
-  def desired_packages do
+  defp desired_packages do
     packages_files()
     |> Enum.flat_map(&extract_package_names/1)
     |> Enum.sort()
@@ -209,36 +244,6 @@ defmodule LocalHexdocs do
     |> Enum.reject(&is_nil(&1))
     |> Enum.reject(&(&1 == ""))
     |> Enum.reject(&String.starts_with?(&1, "#"))
-  end
-
-  @doc """
-  Finds all LocalHexdocs configuration files, generates and de-duplicates a list of all packages in the config
-  files, and then fetches each -- one by one to avoid overloading the Hexdocs.pm server or triggering rate limiting.
-  Saves each package's Hexdocs files in a directory like `~/.hex/docs/hexpm/`.
-
-  Called by `local_docs.exs get`
-  """
-  def fetch_all do
-    stream =
-      desired_packages()
-      |> Task.async_stream(fn lib -> :os.cmd(~c(#{@mix_path} hex.docs fetch #{lib})) end,
-        timeout: @timeout_ms,
-        max_concurrency: @max_concurrency
-      )
-
-    # Expected responses:
-    # "Failed to retrieve package information\nAPI rate limit exceeded for IP [my IP address]\n** (MatchError) no match of right hand side value: nil\n    (hex 2.1.1) lib/mix/tasks/hex.docs.ex:135: Mix.Tasks.Hex.Docs.find_package_latest_version/2\n    (hex 2.1.1) lib/mix/tasks/hex.docs.ex:99: Mix.Tasks.Hex.Docs.fetch_docs/2\n    (mix 1.17.3) lib/mix/task.ex:495: anonymous fn/3 in Mix.Task.run_task/5\n    (mix 1.17.3) lib/mix/cli.ex:96: Mix.CLI.run_task/2\n    /home/mateusz/.asdf/installs/elixir/1.17.3-otp-27/bin/mix:2: (file)"
-    # {:ok, ~c"Couldn't find docs for package with name neotoma or version 1.7.3\n"}
-    # {:ok, ~c"** (Mix) No package with name made_up_library\n"}
-    # {:ok, ~c"Docs already fetched: /home/mateusz/.hex/docs/hexpm/mox/1.2.0\n"}
-    # {:ok, ~c"Docs fetched: /home/mateusz/.hex/docs/hexpm/paginator/1.2.0\n"}
-
-    stream
-    |> Stream.take_while(fn resp -> !rate_limited?(resp) end)
-    |> Stream.each(&display_response/1)
-    |> Enum.to_list()
-    |> process_list()
-    |> IO.inspect(limit: :infinity, printable_limit: :infinity)
   end
 
   defp packages_files do
@@ -331,6 +336,10 @@ defmodule LocalHexdocs do
   defp rate_limited?(resp) do
     elem(resp, 0) == :ok &&
       String.match?(elem(resp, 1) |> to_string(), ~r/rate limit exceeded for IP /)
+  end
+
+  defp hexpm_dir do
+    "~/.hex/docs/hexpm/" |> Path.expand()
   end
 
   # IMPROVE

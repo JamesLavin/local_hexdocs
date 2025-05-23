1ee3efc32b9252ceeec289db83e90d23e27fcb9a
mateusz <james@jameslavin.com>
2025-05-22 23:18:30 -0400

    bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LocalHexdocs.MixProject do
   def project do
     [
       app: :local_hexdocs,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
